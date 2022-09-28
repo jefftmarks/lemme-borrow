446d597ad751e13938a1b235e7fc5330156bcb23
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-28 14:54:40 -0400

    setting up frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom"
+import Login from "./components/Login";
+import Home from "./components/Home"
 
 function App() {
+	const [activeUser, setActiveUser] = useState(null);
+
+	useEffect(() => {
+		fetch("http://localhost:3000/profile")
+			.then((res) => {
+				if (res.ok) {
+					res.json().then((user) => setActiveUser(user))
+				}
+			})
+	}, []);
+
+	if (!activeUser) return <Login />;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+				<Route path="/" exact element={<Home />} />
+			</Routes>
     </div>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Login() {
+	return (
+		<div className="login-container">
+			<h1>Login</h1>
+			<form className="login-form">
+
+			</form>
+		</div>
+	);
+}
+
+export default Login;
