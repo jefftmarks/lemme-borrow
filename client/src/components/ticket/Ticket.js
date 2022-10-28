68285f0b1ac17d3b3dd31571d78306566535e2c7
Jeff Marks <jeff.t.marks@gmail.com>
2022-10-28 13:09:26 -0400

    fixed typo for deployment

--- client/src/components/ticket/Ticket.js
+++ client/src/components/ticket/Ticket.js
@@ -29,7 +29,6 @@ function Ticket({ activeUser }) {
 	// ---------- Action Cable: Create Subscription ----------
 
 	useEffect(() => {
-		console.log("TICKET", params.ticket_id)
 		if (params) {
 			const newChannel = consumer.subscriptions.create({ channel: "TicketChannel", ticket_id: params.ticket_id }, {
 				received(message) {
